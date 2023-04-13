1e4be253f9b6179f780c09b4618adfa0dea786b0
AdalbertoSerna <114277785+AdalbertoSerna@users.noreply.github.com>
2023-04-12 21:37:06 -0500

    Update WeightLoseCalculator.java
    edited the old code in favor of this new one

--- WeightLoseCalculator.java
+++ WeightLoseCalculator.java
@@ -1,33 +1,27 @@
 package edu.utsa.cs3443.macrocounter.Model;
 
 public class WeightLoseCalculator {
-    int currentWeight;
-    int lbsPerWeek;
-    int dailyCalorieGoal = 0;
 
-    public WeightLoseCalculator(int currentWeight, int lbsPerWeek) {
-        this.currentWeight = currentWeight;
+    double lbsPerWeek;
+    int dailyCalorieGoal;
+
+    public WeightLoseCalculator(double lbsPerWeek) {
         this.lbsPerWeek = lbsPerWeek;
     }
 
-    public int getCurrentWeight() {
-        return currentWeight;
-    }
-
-    public void setCurrentWeight(int currentWeight) {
-        this.currentWeight = currentWeight;
-    }
-
-    public int getLbsPerWeek() {
-        return lbsPerWeek;
-    }
-
-    public void setLbsPerWeek(int lbsPerWeek) {
-        this.lbsPerWeek = lbsPerWeek;
-    }
-    public int setDailyCalorieGoal(int lbsPerWeek){
-//NOTE: We cannot exceed 2lbs per week anything more can cause actually be very bad for your health
-        dailyCalorieGoal = 2000 - lbsPerWeek * 500;
+    public int getDailyCalorieGoal() {
         return dailyCalorieGoal;
     }
+
+    public void setDailyCalorieGoal(int dailyCalorieGoal) {
+        this.dailyCalorieGoal = dailyCalorieGoal;
+    }
+    public int setDailyCalorieGoal(){
+//NOTE: We cannot exceed 2lbs per week anything more can cause actually be very bad for your health
+        dailyCalorieGoal = (int) (2000 - lbsPerWeek * 500);
+
+        return dailyCalorieGoal;
+    }
+
+
 }
